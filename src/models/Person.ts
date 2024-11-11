1141db90c548b0ec6722e903f35853778146bf4c
Koen <fuchsk@hva.nl>
2024-11-11 23:39:37 +0100

    updated registation.ts and Person.ts to check if email already exists

--- src/models/Person.ts
+++ src/models/Person.ts
@@ -19,7 +19,12 @@ export class Person {
     }
 
     public async setPerson(): Promise<void> {
-        await api.queryDatabase(`INSERT INTO user (name, email, password) VALUES ('${this._name}', '${this._email}', '${this._password}')`);
+        try {
+            await api.queryDatabase(`INSERT INTO user (name, email, password) VALUES ('${this._name}', '${this._email}', '${this._password}')`);
+        }
+        catch (reason) {
+            console.error(reason);
+        }
     }
 
     public static async removePerson(removingPerson: string): Promise<void> {
@@ -33,5 +38,21 @@ export class Person {
             persons.push(new Person(row.name, row.email, row.password));
         }
         return persons;
+    }
+
+    public static async checkIfEmailExists(emailInput: string): Promise<boolean> {
+        try {
+            let emailExists: boolean = false;
+            const emails: PersonQueryResult[] =
+            await api.queryDatabase(`SELECT email FROM user WHERE LOWER(email) = '${emailInput.toLowerCase()}'`) as PersonQueryResult[];
+            if (emails.length > 0) {
+                emailExists = true;
+            }
+            return emailExists;
+        }
+        catch (reason) {
+            console.error(reason);
+            throw new Error(`Failed to check if email exists: ${reason}`);
+        }
     }
 }
--- src/views/registration.ts
+++ src/views/registration.ts
@@ -1,14 +1,24 @@
+import "../hicConfig";
+import { Person } from "../models/Person";
+
 const registrationButton: HTMLButtonElement = document.querySelector("#registration-button")!;
 
-registrationButton.addEventListener("click", event => {
+// als er op de knop wordt geklikt vraagt de functie of de inputs kloppen en geeft deze door aan de Person class
+registrationButton.addEventListener("click", async event => {
     event.preventDefault();
 
-    const validInputs: boolean = validateInputs();
+    const userName: HTMLInputElement = document.querySelector("#input-username")!;
+    const email: HTMLInputElement = document.querySelector("#input-email")!;
+    const password: HTMLInputElement = document.querySelector("#input-password")!;
+
+    const validInputs: boolean = await validateInputs(userName, email, password);
     if (validInputs) {
-        alert("Registration successful!");
+        const person: Person = new Person(userName.value, email.value, password.value);
+        void person.setPerson();
     }
 });
 
+// geeft error message als input niet klopt
 function setErrorMessage(input: HTMLInputElement, message: string): void {
     const errorMessage: HTMLDivElement = input.parentElement as HTMLDivElement;
     const errorText: HTMLParagraphElement = errorMessage.querySelector(".form-error")!;
@@ -16,6 +26,7 @@ function setErrorMessage(input: HTMLInputElement, message: string): void {
     errorText.innerText = message;
 }
 
+// haalt error weg input klopt
 function clearErrorMessage(input: HTMLInputElement): void {
     const errorMessage: HTMLDivElement = input.parentElement as HTMLDivElement;
     const errorText: HTMLParagraphElement = errorMessage.querySelector(".form-error")!;
@@ -23,17 +34,16 @@ function clearErrorMessage(input: HTMLInputElement): void {
     errorText.innerText = "";
 }
 
+// checkt of email klopt en geeft een boolean terug
 function validEmail(email: string): boolean {
     const emailRegex: RegExp = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
     ;
     return emailRegex.test(email);
 }
 
-function validateInputs(): boolean {
+// checkt of inputs kloppen en geeft een boolean terug
+async function validateInputs(userName: HTMLInputElement, email: HTMLInputElement, password: HTMLInputElement): Promise<boolean> {
     let valid: boolean = true;
-    const userName: HTMLInputElement = document.querySelector("#input-username")!;
-    const email: HTMLInputElement = document.querySelector("#input-email")!;
-    const password: HTMLInputElement = document.querySelector("#input-password")!;
 
     if (userName.value === "") {
         setErrorMessage(userName, "Please enter a username.");
@@ -51,7 +61,14 @@ function validateInputs(): boolean {
         valid = false;
     }
     else {
-        clearErrorMessage(email);
+        const emailExists: boolean = await Person.checkIfEmailExists(email.value);
+        if (emailExists) {
+            setErrorMessage(email, "Email address is already in use.");
+            valid = false;
+        }
+        else {
+            clearErrorMessage(email);
+        }
     }
     if (password.value === "") {
         setErrorMessage(password, "Please enter a password.");
