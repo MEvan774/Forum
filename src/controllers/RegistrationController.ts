d7477487b3c7e63a56a769be59d599acf0232996
Koen <fuchsk@hva.nl>
2024-11-14 22:55:36 +0100

    placed validation function in the registration controller in RegistrationController.ts

--- src/controllers/RegistrationController.ts
+++ src/controllers/RegistrationController.ts
@@ -9,11 +9,72 @@ export class RegisterController extends Controller {
     public render(): void {
         this.view.addEventListener("click", (event: Event) => {
             event.preventDefault();
-            this.onClickRegister();
+            void this.onClickRegister();
         });
     }
 
-    private onClickRegister(): void {
-        void User.registrateUser();
+    private async validateInputs(userName: HTMLInputElement, email: HTMLInputElement, password: HTMLInputElement): Promise<boolean> {
+        let valid: boolean = true;
+
+        if (userName.value === "") {
+            User.setErrorMessage(userName, "Please enter a username.");
+            valid = false;
+        }
+        else if (userName.value.includes("@")) {
+            User.setErrorMessage(userName, "Username cannot contain '@'.");
+            valid = false;
+        }
+        else {
+            const userNameExists: boolean = await User.checkIfUsernameExists(userName.value);
+            if (userNameExists) {
+                User.setErrorMessage(userName, "Username is already in use.");
+                valid = false;
+            }
+            else {
+                User.clearErrorMessage(userName);
+            }
+        }
+        if (email.value === "") {
+            User.setErrorMessage(email, "Please enter an email address.");
+            valid = false;
+        }
+        else if (!User.validEmail(email.value)) {
+            User.setErrorMessage(email, "Please enter a valid email address.");
+            valid = false;
+        }
+        else {
+            const emailExists: boolean = await User.checkIfEmailExists(email.value);
+            if (emailExists) {
+                User.setErrorMessage(email, "Email address is already in use.");
+                valid = false;
+            }
+            else {
+                User.clearErrorMessage(email);
+            }
+        }
+        if (password.value === "") {
+            User.setErrorMessage(password, "Please enter a password.");
+            valid = false;
+        }
+        else if (password.value.length < 8) {
+            User.setErrorMessage(password, "Password must be at least 8 characters long.");
+            valid = false;
+        }
+        else {
+            User.clearErrorMessage(password);
+        }
+        return valid;
+    };
+
+    private async onClickRegister(): Promise<void> {
+        const userName: HTMLInputElement = document.querySelector("#input-username")!;
+        const email: HTMLInputElement = document.querySelector("#input-email")!;
+        const password: HTMLInputElement = document.querySelector("#input-password")!;
+
+        const validRegistrationInput: boolean = await this.validateInputs(userName, email, password);
+        if (validRegistrationInput) {
+            const user: User = new User(userName.value, email.value, password.value);
+            void user.setUser();
+        }
     }
 }
--- src/models/User.ts
+++ src/models/User.ts
@@ -76,6 +76,28 @@ export class User {
         }
     }
 
+    // geeft error message als input niet klopt
+    public static setErrorMessage(input: HTMLInputElement, message: string): void {
+        const errorMessage: HTMLDivElement = input.parentElement as HTMLDivElement;
+        const errorText: HTMLParagraphElement = errorMessage.querySelector(".form-error")!;
+
+        errorText.innerText = message;
+    }
+
+    // haalt error weg input klopt
+    public static clearErrorMessage(input: HTMLInputElement): void {
+        const errorMessage: HTMLDivElement = input.parentElement as HTMLDivElement;
+        const errorText: HTMLParagraphElement = errorMessage.querySelector(".form-error")!;
+
+        errorText.innerText = "";
+    }
+
+    // checkt of email klopt en geeft een boolean terug
+    public static validEmail(email: string): boolean {
+        const emailRegex: RegExp = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return emailRegex.test(email);
+    }
+
     /*
     * kijkt of gebruikersnaam al bestaat in de database
     * kan gebruikt worden bij zowel de regristratie als de inlog
@@ -94,101 +116,6 @@ export class User {
             console.error(error);
             throw new Error(`Failed to check if username exists: ${error}`);
         }
-    }
-
-    /*
-    * registreet de gebruiker als de inputs kloppen
-    * wordt geroepen vanuit de registratie controller zodra registreer knop wordt ingedrukt
-    **/
-    public static async registrateUser(): Promise<void> {
-        const userName: HTMLInputElement = document.querySelector("#input-username")!;
-        const email: HTMLInputElement = document.querySelector("#input-email")!;
-        const password: HTMLInputElement = document.querySelector("#input-password")!;
-
-        const validInputs: boolean = await validateInputs(userName, email, password);
-        if (validInputs) {
-            const user: User = new User(userName.value, email.value, password.value);
-            void user.setUser();
-            void User.sendEmail(userName.value, email.value);
-            User.setCurrentlyLoggedInUser(userName.value);
-            window.location.href = "/index.html";
-        }
-
-        // geeft error message als input niet klopt
-        function setErrorMessage(input: HTMLInputElement, message: string): void {
-            const errorMessage: HTMLDivElement = input.parentElement as HTMLDivElement;
-            const errorText: HTMLParagraphElement = errorMessage.querySelector(".form-error")!;
-
-            errorText.innerText = message;
-        }
-
-        // haalt error weg input klopt
-        function clearErrorMessage(input: HTMLInputElement): void {
-            const errorMessage: HTMLDivElement = input.parentElement as HTMLDivElement;
-            const errorText: HTMLParagraphElement = errorMessage.querySelector(".form-error")!;
-
-            errorText.innerText = "";
-        }
-
-        // checkt of email klopt en geeft een boolean terug
-        function validEmail(email: string): boolean {
-            const emailRegex: RegExp = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-            return emailRegex.test(email);
-        }
-
-        // checkt of inputs kloppen en geeft een boolean terug
-        async function validateInputs(userName: HTMLInputElement, email: HTMLInputElement, password: HTMLInputElement): Promise<boolean> {
-            let valid: boolean = true;
-
-            if (userName.value === "") {
-                setErrorMessage(userName, "Please enter a username.");
-                valid = false;
-            }
-            else if (userName.value.includes("@")) {
-                setErrorMessage(userName, "Username cannot contain '@'.");
-                valid = false;
-            }
-            else {
-                const userNameExists: boolean = await User.checkIfUsernameExists(userName.value);
-                if (userNameExists) {
-                    setErrorMessage(userName, "Username is already in use.");
-                    valid = false;
-                }
-                else {
-                    clearErrorMessage(userName);
-                }
-            }
-            if (email.value === "") {
-                setErrorMessage(email, "Please enter an email address.");
-                valid = false;
-            }
-            else if (!validEmail(email.value)) {
-                setErrorMessage(email, "Please enter a valid email address.");
-                valid = false;
-            }
-            else {
-                const emailExists: boolean = await User.checkIfEmailExists(email.value);
-                if (emailExists) {
-                    setErrorMessage(email, "Email address is already in use.");
-                    valid = false;
-                }
-                else {
-                    clearErrorMessage(email);
-                }
-            }
-            if (password.value === "") {
-                setErrorMessage(password, "Please enter a password.");
-                valid = false;
-            }
-            else if (password.value.length < 8) {
-                setErrorMessage(password, "Password must be at least 8 characters long.");
-                valid = false;
-            }
-            else {
-                clearErrorMessage(password);
-            }
-            return valid;
-        };
     }
 
     public static async sendEmail(inputName: string, email: string): Promise<void> {
