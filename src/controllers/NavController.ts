3679d5418aa787fdecbfba88df8d2cf816fc161b
Koen <fuchsk@hva.nl>
2024-11-18 19:59:12 +0100

    created navController to display log out or register and login options in NavController.ts

---
+++ src/controllers/NavController.ts
@@ -0,0 +1,76 @@
+import { Controller } from "./Controller";
+import { session } from "@hboictcloud/api";
+import { LoggedIn } from "../models/LoggedIn";
+
+export class NavController extends Controller {
+    public constructor(view: HTMLElement) {
+        super(view);
+    }
+
+    public render(): void {
+        this.checkIfLoggedIn();
+    }
+
+    /**
+     * kijkt of er een gebruiker is ingelogd
+     * leest uit session of het waar is
+     * als het waar is wordt de logout button toegevoegd
+     * als het niet waar is worden de login en registratie buttons toegevoegd
+     */
+    private checkIfLoggedIn(): void {
+        try {
+            const loggedIn: LoggedIn = session.get("LoggedIn") as LoggedIn;
+            if (loggedIn.isLoggedIn) {
+                console.log(`User ${loggedIn.userName} is logged in.`);
+                this.addLogoutButton(this.view);
+            }
+            else {
+                console.log("No user is logged in.");
+                this.addLoginAndRegisterButtons(this.view);
+            }
+        }
+        catch {
+            this.addLoginAndRegisterButtons(this.view);
+        }
+    }
+
+    /**
+     * voegt de logout button toe aan de navigatie
+     * als de logout button wordt geklikt wordt er een confirmatie scherm getoond
+     * als de gebruiker op de confirmatie knop klikt wordt de gebruiker uitgelogd
+     */
+    private addLogoutButton(navigationContainer: HTMLElement): void {
+        const logoutButton: HTMLAnchorElement = document.createElement("a");
+        logoutButton.textContent = "Log uit";
+        logoutButton.addEventListener("click", () => {
+            document.body.classList.add("show-logout", "show-overlay");
+            const confirmButton: HTMLButtonElement = document.getElementById("logout") as HTMLButtonElement;
+            confirmButton.addEventListener("click", () => {
+                session.remove("LoggedIn");
+                window.location.href = "/index.html";
+            });
+
+            const cancelLogoutButton: HTMLButtonElement = document.querySelector(".cancel-logout-button") as HTMLButtonElement;
+            cancelLogoutButton.addEventListener("click", () => {
+                document.body.classList.remove("show-logout", "show-overlay");
+            });
+        });
+        navigationContainer.appendChild(logoutButton);
+    }
+
+    /**
+     * voegt de login en registratie buttons toe aan de navigatie
+     */
+    private addLoginAndRegisterButtons(navigationContainer: HTMLElement): void {
+        const loginButton: HTMLAnchorElement = document.createElement("a");
+        loginButton.href = "/login.html";
+        loginButton.textContent = "Log in";
+
+        const registerButton: HTMLAnchorElement = document.createElement("a");
+        registerButton.href = "/registration.html";
+        registerButton.textContent = "Registreren";
+
+        navigationContainer.appendChild(loginButton);
+        navigationContainer.appendChild(registerButton);
+    }
+}
--- src/views/index.ts
+++ src/views/index.ts
@@ -1,73 +1,11 @@
 import "../hicConfig";
-import { session } from "@hboictcloud/api";
-import { LoggedIn } from "../models/LoggedIn";
 import { HomeController } from "../controllers/HomeController";
+import { NavController } from "../controllers/NavController";
 
-/**
- * kijkt of er een gebruiker is ingelogd
- * leest uit session of het waar is
- * als het waar is wordt de logout button toegevoegd
- * als het niet waar is worden de login en registratie buttons toegevoegd
- */
-function checkIfUserLoggedIn(): void {
-    const navigationContainer: HTMLDivElement = document.querySelector(".nav-links")!;
+const navView: HTMLElement = document.querySelector(".nav-links")!;
+const navController: NavController = new NavController(navView);
+navController.render();
 
-    try {
-        const loggedIn: LoggedIn = session.get("LoggedIn") as LoggedIn;
-        if (loggedIn.isLoggedIn) {
-            console.log(`User ${loggedIn.userName} is logged in.`);
-            addLogoutButton(navigationContainer);
-        }
-        else {
-            console.log("No user is logged in.");
-            addLoginAndRegisterButtons(navigationContainer);
-        }
-    }
-    catch {
-        addLoginAndRegisterButtons(navigationContainer);
-    }
-}
-
-/**
- * voegt de logout button toe aan de navigatie
- * als de logout button wordt geklikt wordt er een confirmatie scherm getoond
- * als de gebruiker op de confirmatie knop klikt wordt de gebruiker uitgelogd
- */
-function addLogoutButton(navigationContainer: HTMLDivElement): void {
-    const logoutButton: HTMLAnchorElement = document.createElement("a");
-    logoutButton.textContent = "Log uit";
-    logoutButton.addEventListener("click", () => {
-        document.body.classList.add("show-logout", "show-overlay");
-        const confirmButton: HTMLButtonElement = document.getElementById("logout") as HTMLButtonElement;
-        confirmButton.addEventListener("click", () => {
-            session.remove("LoggedIn");
-            window.location.href = "/index.html";
-        });
-
-        const cancelLogoutButton: HTMLButtonElement = document.querySelector(".cancel-logout-button") as HTMLButtonElement;
-        cancelLogoutButton.addEventListener("click", () => {
-            document.body.classList.remove("show-logout", "show-overlay");
-        });
-    });
-    navigationContainer.appendChild(logoutButton);
-}
-
-/**
- * voegt de login en registratie buttons toe aan de navigatie
- */
-function addLoginAndRegisterButtons(navigationContainer: HTMLDivElement): void {
-    const loginButton: HTMLAnchorElement = document.createElement("a");
-    loginButton.href = "/login.html";
-    loginButton.textContent = "Log in";
-
-    const registerButton: HTMLAnchorElement = document.createElement("a");
-    registerButton.href = "/registration.html";
-    registerButton.textContent = "Registreren";
-
-    navigationContainer.appendChild(loginButton);
-    navigationContainer.appendChild(registerButton);
-}
-checkIfUserLoggedIn();
-const view: HTMLElement = document.querySelector(".questions-container")!;
-const controller: HomeController = new HomeController(view);
-controller.render();
+const homeView: HTMLElement = document.querySelector(".questions-container")!;
+const homeController: HomeController = new HomeController(homeView);
+homeController.render();
