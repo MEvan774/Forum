12ecc7a3ef62ed5e722960935029b2867ce36128
Sjoerd Brouwer <sjoerd.brouwer@hva.nl>
2024-11-19 10:39:04 +0100

    Added navController and questionController to question view

---
+++ src/views/question.ts
@@ -0,0 +1,11 @@
+import "../hicConfig";
+import { QuestionController } from "../controllers/QuestionController";
+import { NavController } from "../controllers/NavController";
+
+const navView: HTMLElement = document.querySelector(".nav-links")!;
+const navController: NavController = new NavController(navView);
+navController.render();
+
+const questionView: HTMLElement = document.querySelector(".question-detail-container") as HTMLElement;
+const questionController: QuestionController = new QuestionController(questionView);
+questionController.render();
