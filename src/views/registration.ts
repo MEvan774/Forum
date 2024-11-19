d97ebb7c02bb52bbf174aa5fcbf855dee0103683
Koen <fuchsk@hva.nl>
2024-11-19 18:37:30 +0100

    fixed selecting correct HTMLElement in login.ts

--- src/views/registration.ts
+++ src/views/registration.ts
@@ -2,7 +2,7 @@ import "../hicConfig";
 import { RegisterController } from "../controllers/RegistrationController";
 import { NavController } from "../controllers/NavController";
 
-const navView: HTMLElement = document.querySelector("navbar")!;
+const navView: HTMLElement = document.querySelector(".navbar")!;
 const navController: NavController = new NavController(navView);
 navController.render();
 
