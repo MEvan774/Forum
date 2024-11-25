03cebc7d0a80e6bf5cbb0a76cb462b6cbf3cda76
Sjoerd Brouwer <sjoerd.brouwer@hva.nl>
2024-11-25 03:37:50 +0100

    Added eventlistener to generated button and placeholder text to input

--- src/controllers/QuestionCreateController.ts
+++ src/controllers/QuestionCreateController.ts
@@ -10,27 +10,44 @@ export class QuestionCreateController extends Controller {
     public render(): void {
         this.view.addEventListener("click", () => {
             this.createForm();
+            const createQuestionButton: HTMLButtonElement =
+            document.querySelector(".button-create-question")!;
+            createQuestionButton.addEventListener("click", () => {
+                this.createQuestion();
+            });
         });
     }
 
+    // Creates a form with a text input field and a submit button
     private createForm(): void {
         const formContainer: HTMLDivElement = this.view.parentElement as HTMLDivElement;
 
+        // Removes the original button
         const createFormButton: HTMLButtonElement = document.querySelector(".button-create-form")!;
-        createFormButton.remove(); // Removes the original button
+        createFormButton.remove();
 
-        // Adds the form and the child elements
+        // Creates form element
         const formElement: HTMLFormElement = document.createElement("form");
         formElement.classList.add(".question-form");
-        const labelElement: HTMLLabelElement = document.createElement("label");
+
+        // Creates input element
         const inputElement: HTMLInputElement = document.createElement("input");
+        inputElement.setAttribute("placeholder", "Schrijf hier je vraag!"); // Adds placeholder text to input
+
+        // Creates button
         const createQuestionButton: HTMLButtonElement = document.createElement("button");
         createQuestionButton.classList.add(".button-create-question");
+        createQuestionButton.innerText = "Submit!";
 
-        formElement.appendChild(labelElement);
+        // Adds the input and button as child elements to the form
         formElement.appendChild(inputElement);
         formElement.appendChild(createQuestionButton);
 
+        // Adds the form as a child to the main container
         formContainer.appendChild(formElement);
+    }
+
+    private createQuestion(): void {
+
     }
 }
