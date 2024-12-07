fd4611f2a43fc8e69c10242a3524e250e4f60b5d
Milan Breuren <milan.breuren@hva.nl>
2024-12-07 12:02:25 +0100

    Added the html page and css for the edit profile, no functionality has yet been added

---
+++ src/views/edit-profile.ts
@@ -0,0 +1,21 @@
+import "../hicConfig";
+import { HomeController } from "../controllers/HomeController";
+import { NavController } from "../controllers/NavController";
+import { FooterController } from "../controllers/FooterController";
+import { QuestionCreateController } from "../controllers/QuestionCreateController";
+
+const navView: HTMLElement = document.querySelector(".navbar")!;
+const navController: NavController = new NavController(navView);
+navController.render();
+
+const homeView: HTMLElement = document.querySelector(".questions-container")!;
+const homeController: HomeController = new HomeController(homeView);
+homeController.render();
+
+const footerView: HTMLElement = document.querySelector(".footerbar")!;
+const footerController: FooterController = new FooterController(footerView);
+footerController.render();
+
+const questionCreateView: HTMLElement = document.querySelector(".button-create-form")!;
+const questionCreateController: QuestionCreateController = new QuestionCreateController(questionCreateView);
+questionCreateController.render();
